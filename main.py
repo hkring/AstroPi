c6f3f2a1bf31bc17d47d76fc4116ac4b0a625e3f
hkring <hkring@ptc.com>
2025-02-05 21:04:32 -0500

    calculate speed based on geo coordinates

--- main.py
+++ main.py
@@ -44,12 +44,60 @@ def calculateGSDvalue(altidue: int) -> int:
     '''
     Calculates the ground sampling distance (GSD)
 
-    Args altitude (int): ISS orbits between 370000 - 460000 [meter]
+    Args: 
+        altitude (int): ISS orbits between 370000 - 460000 [meter]
 
-    Return GSD (int): scale factor [cm/pixel]   
+    Return: 
+        ground sampling distance (int): scale factor [cm/pixel]   
     '''
     grounddistance = 2 * math.tan(beta) * altidue * 100
     return int(grounddistance/widthpx)
+
+def convert_to_degree(dmscoordinate) -> float:
+    return dmscoordinate[0] + (dmscoordinate[1] / 60) + (dmscoordinate[2] / 3600)
+
+def get_sign(refchar: str) -> float:
+    match refchar:
+        case "N" | "E":
+            return 1.0
+        case "S" | "W":
+            return -1.0
+        case _:
+            return 1.0 
+
+def degree_to_radian(ref: str, degree: float) -> float:
+    return get_sign(ref) * degree * math.pi / 180
+     
+def measure_distance(image_1, image_2) -> float:
+    '''
+    Calculate the the great-cicle distance between two point on the basis of spherical earth
+
+    Args:
+
+    '''
+    with open(image_1, 'rb') as image_file:
+        img = Image(image_file)
+        latA = convert_to_degree(img.get('gps_latitude'))
+        latrefA = img.get('gps_latitude_ref')
+        lonA = convert_to_degree(img.get('gps_longitude'))
+        lonrefA = img.get('gps_longitude_ref')
+    
+    with open(image_2, 'rb') as image_file:
+        img = Image(image_file)
+        latB = convert_to_degree(img.get('gps_latitude'))
+        latrefB = img.get('gps_latitude_ref')
+        lonB = convert_to_degree(img.get('gps_longitude'))
+        lonrefB = img.get('gps_longitude_ref')   
+
+    R = 6378.137 #Radius earth in km
+    dlat = degree_to_radian(latrefB,latB) - degree_to_radian(latrefA,latA)
+    dlon = degree_to_radian(lonrefB,lonB) - degree_to_radian(lonrefA,lonA)
+    #a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(degree_to_radian(latrefA,latA)) * math.cos(degree_to_radian(latrefB,latB)) * math.sin(dlon/2)*math.sin(dlon/2)
+    #c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    a = 0.5 - math.cos(dlat)/2 + math.cos(degree_to_radian(latrefA,latA)) * math.cos(degree_to_radian(latrefB,latB)) * (1-math.cos(dlon))/2
+    c = 2* math.asin(math.sqrt(a))
+    return R*c
+    
 
 #----------------------------------------------------- Main Logic -------------------------------------------------------
 
@@ -64,17 +112,24 @@ while datetime.now().timestamp()- starttime < 30:
     # Holger comment : round timestamp to compare with an integer 
     if flag == 0 and round( datetime.now().timestamp() - starttime) == 15:
         logger.debug('15 seconds')
+        # Holger comment : extract geo location photo 1
         coordinate = get_gps_DMS_String(image_1)
         logger.debug(f"{image_1} coordinates {coordinate}")        
         flag = 1
 
+# Holger comment : extract geo location photo 2
 coordinate = get_gps_DMS_String(image_2)
 logger.debug(f"{image_2} coordinates {coordinate}")
-#Holger comment: open image files and read timestamp meta data  
+
+# Holger comment : read the timestamp the photos have been taken
 time_difference = get_time_difference(image_1,image_2)
-logger.debug(f"The time difference between two photos is {time_difference} seconds")
-#Holger comment: assuming an altitude of 435250  
-gds = calculateGSDvalue(435250)
-logger.debug(f"GSD Scaling factor is {gds}")
+logger.debug(f"The time difference between two photos is {time_difference} in seconds")
+
+# Holger comment : calculate distance between geo coordinates
+distance = measure_distance(image_1, image_2)
+logger.debug(f"The ground distance between two coordinate is {distance} in km")
+
+speed = distance / time_difference
+logger.debug(f"Result speed calculation {speed} in kmps")
 
 logger.debug('30 seconds')
